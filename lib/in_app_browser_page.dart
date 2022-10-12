fa05e71e34a838d1689c94345b08fc538d1e375e
Felix Rzayev <felixrzayev@Samirs-MacBook-Pro.local>
2022-10-12 16:10:50 +0400

    Revision 6

--- lib/in_app_browser_page.dart
+++ lib/in_app_browser_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
@@ -17,40 +19,51 @@ class _InAppBrowserPageState extends State<InAppBrowserPage> {
   late InAppWebViewController webView;
   GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
+  Future<bool> _onBack() async {
+    if (await webView.canGoBack()) {
+      webView.goBack(); // perform webview back operation
+      return false;
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        automaticallyImplyLeading: false,
-        toolbarHeight: 0,
-        elevation: 0,
-        centerTitle: true,
-      ),
-      body: Stack(
-        children: [
-          InAppWebView(
-            initialUrlRequest: URLRequest(
-              url: Uri.parse(widget.url),
+    return WillPopScope(
+      onWillPop: _onBack,
+      child: Scaffold(
+        key: scaffoldKey,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          automaticallyImplyLeading: false,
+          toolbarHeight: 0,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        body: Stack(
+          children: [
+            InAppWebView(
+              initialUrlRequest: URLRequest(
+                url: Uri.parse(widget.url),
+              ),
+              onWebViewCreated: ((InAppWebViewController controller) {
+                webView = controller;
+              }),
+              onProgressChanged:
+                  (InAppWebViewController controller, int progress) {
+                setState(() {
+                  _progress = progress / 100;
+                });
+              },
             ),
-            onWebViewCreated: ((InAppWebViewController controller) {
-              webView = controller;
-            }),
-            onProgressChanged:
-                (InAppWebViewController controller, int progress) {
-              setState(() {
-                _progress = progress / 100;
-              });
-            },
-          ),
-          _progress < 1
-              ? SizedBox(
-                  height: 3,
-                  child: LinearProgressIndicator(value: _progress),
-                )
-              : SizedBox()
-        ],
+            _progress < 1
+                ? SizedBox(
+                    height: 3,
+                    child: LinearProgressIndicator(value: _progress),
+                  )
+                : const SizedBox()
+          ],
+        ),
       ),
     );
   }
